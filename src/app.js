7002bf9f628111ad5a840a4eb446d27f866a5b8d
alexmarin86 <alexeu86@gmail.com>
2022-11-26 11:03:14 +0100

    versión 1.0

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const inputBuscar = document.querySelector('#buscar')
 const inputCantidad = document.querySelector('#cantidad')
 const botonBuscar = document.querySelector('.enviar')
+const divAlerta = document.querySelector('.alerta')
+const elemTabla = document.querySelector('table')
 
 const obtenerDatos = async () => {
 	try {
@@ -11,17 +13,65 @@ const obtenerDatos = async () => {
 	}
 }
 
-const filtrarDatos = async (busqueda) => {
+const filtrarDatos = async (busqueda, cantidad) => {
 	const datos = await obtenerDatos()
-	const resultadoBusqueda = datos.filter((art) => {
+	divAlerta.textContent = ''
+	elemTabla.innerHTML = ''
+	const resultadoBusqueda = datos.find((art) => {
 		return art.refArt == busqueda
 	})
-	return resultadoBusqueda
+
+	if (!resultadoBusqueda) {
+		divAlerta.textContent =
+			'No se han encontrado resultados. Introduzca otra referencia.'
+		return
+	}
+	if (!cantidad || cantidad < 1) {
+		divAlerta.textContent = 'Debe introducir una cantidad.'
+		return
+	}
+	if (cantidad > 20) {
+		divAlerta.innerHTML = `Consulte con un comercial para consultas superiores a 20 unidades. <a href="tel:+34963267365" class="text-zinc-300 underline">963 267 365</a>`
+		return
+	}
+
+	const { refArt, codigoEan, nombreArt, stock, prodTransito } =
+		resultadoBusqueda
+	elemTabla.innerHTML = `
+		<tr>
+				<th class="border-2 p-1">Referencia Art</th>
+				<th class="border-2 p-1">Código EAN</th>
+				<th class="border-2 p-1">Nombre Art</th>
+				<th class="border-2 p-1">Stock</th>
+				<th class="border-2 p-1">Producción y tránsito</th>
+			</tr>
+			<tr>
+				<td class="border-2 p-1">${refArt}</td>
+				<td class="border-2 p-1">${codigoEan}</td>
+				<td class="border-2 p-1">${nombreArt}</td>
+				<td class="border-2 p-1 resalte">${hayStock(stock, cantidad)}</td>
+				<td class="border-2 p-1">${prodTransito}</td>
+			</tr>
+	`
+	const celdaResalte = document.querySelector('.resalte')
+	if (celdaResalte.textContent == 'Disponible') {
+		celdaResalte.style.backgroundColor = 'lime'
+		return
+	}
+	celdaResalte.style.backgroundColor = 'tomato'
+	console.log(resultadoBusqueda)
+}
+function hayStock(numero, cantidad) {
+	if (numero - cantidad >= 0) {
+		return 'Disponible'
+	}
+	return 'Agotado'
 }
 // console.log(filtrarDatos('0c08A'))
 
 botonBuscar.addEventListener('click', async (e) => {
 	e.preventDefault()
 	const busqueda = inputBuscar.value
-	console.log(filtrarDatos(busqueda))
+	const cantidad = inputCantidad.value
+	filtrarDatos(busqueda, cantidad)
 })
